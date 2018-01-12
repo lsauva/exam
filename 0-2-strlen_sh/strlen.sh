8b9c5909ff72aa4c0409f24098f44a50d30a5a94
Laurent SAUVAGE <lsauvage@student.42.fr>
2018-01-12 12:26:57 +0100

    examshell

---
+++ 0-2-strlen_sh/strlen.sh
@@ -0,0 +1,17 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    strlen.sh                                          :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: lsauvage <lsauvage@student.42.fr>          +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2018/01/11 15:17:52 by lsauvage          #+#    #+#              #
+#    Updated: 2018/01/12 12:25:53 by lsauvage         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+#!/bin/sh
+# affichage de la variable FT_LINE | compte les caracteres | supprime les
+# espaces en debut de ligne
+# resultat OK en utilisant la commande 'source strlen.sh'
+echo -n $FT_LINE | wc -m | sed 's/^[ \t]*//'
