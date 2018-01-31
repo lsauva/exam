322562fa014369130087159d4a8d1531ae57eda9
Laurent SAUVAGE <lsauvage@student.42.fr>
2018-01-31 19:08:58 +0100

    add rev_wstr

---
+++ 4-0-rev_wstr/rev_wstr2.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rev_wstr2.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lsauvage <lsauvage@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/01/31 11:29:37 by lsauvage          #+#    #+#             */
+/*   Updated: 2018/01/31 19:06:35 by lsauvage         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int     ft_isblank(int c)
+{
+    return (c == ' ' || c == '\t');
+}
+
+int     ft_strlen(char *str)
+{
+    int i;
+
+    i = 0;
+    while (str[i])
+        i++;
+    return (i);
+}
+
+void    rev_wstr(char *str)
+{
+    int wc;
+    int i;
+    int len;
+    int a;
+
+    wc = 1;
+    i = 0;
+    len = ft_strlen(str);
+    while (--len && !ft_isblank(str[len])
+        i++;
+}
+
+int     main(int ac, char **av)
+{
+    if (ac == 2 && *av[1])
+        rev_wstr(av[1]);
+    write(1, "\n", 1);
+    return (0);
+}
