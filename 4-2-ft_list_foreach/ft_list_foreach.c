a10ffba3c03d660395ca7f1f177c8f65d632bec2
Laurent Sauvage <lsauvage@student.42.fr>
2018-04-10 11:34:49 +0200

    exam : ft_list_foreach

---
+++ 4-2-ft_list_foreach/ft_list_foreach.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_list_foreach.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lsauvage <lsauvage@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/02/12 16:10:20 by lsauvage          #+#    #+#             */
+/*   Updated: 2018/02/12 16:23:53 by lsauvage         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_list.h"
+
+void	ft_list_foreach(t_list *begin_list, void (*f)(void *))
+{
+	t_list *list;
+
+	list = begin_list;
+	while (list)
+	{
+		(*f)(list->data);
+		list = list->next;
+	}
+}
