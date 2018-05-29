f2869115cedf558281628ee58c357c3405d665f8
Laurent SAUVAGE <lsauvage@student.42.fr>
2018-05-29 16:38:19 +0200

    ajout ex fail ou ex incomplets

---
+++ options/options.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   options.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: exam <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/05/29 11:24:34 by exam              #+#    #+#             */
+/*   Updated: 2018/05/29 11:38:14 by exam             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int		options()
+
+	a = ascii 97
+	z = ascii 122
+00000000 00000000 00000000 00000000
+******zy xwvutsrq ponmlkji hgfedcba
+
+	tab[6] = z
+	tab[7] = y
+	tab[8] = ' '
+	tab[9] = x
+	tab[34] = a
+
+int		is_alpha(char c)
+{
+	return (c >= 'a' && c <= 'z');
+}
+
+si ac == 1 || *s == h affiche usage
+si *s == - && !is_alpha(*++s) affiche Invalid Option
+sinon 
+
